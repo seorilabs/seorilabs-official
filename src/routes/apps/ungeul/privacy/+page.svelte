5ecaf19774a0227842299dd0db9daea173fdb905
Ilhwan Seo <wellknowngeek@gmail.com>
2026-08-21 20:01:14 +0900

    운글 개인정보처리방침을 추가한다
    - 한·영 제품별 개인정보처리방침을 추가한다
    - 사이트맵에 운글 방침 URL을 등록한다
    - 기기 저장과 가명 서버 처리 범위를 구분한다

---
+++ src/routes/apps/ungeul/privacy/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import PrivacyPage from '$lib/PrivacyPage.svelte';
+	import { ungeulPrivacyContent } from '$lib/ungeulPrivacyContent';
+</script>
+
+<PrivacyPage localeKey="ko" contentOverride={ungeulPrivacyContent.ko} productSlug="ungeul" />
