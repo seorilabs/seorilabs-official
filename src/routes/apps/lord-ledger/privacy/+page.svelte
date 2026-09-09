41e6899ca943c11e1b585aa46f1ac728a401666f
Ilhwan <ih@seorilabs.com>
2026-09-09 11:56:23 +0900

    - 삼국지 풍운록 개인정보와 계정 삭제 안내 추가 (#18)
    - 한국어·영어 정책 및 외부 삭제 요청 경로 제공

---
+++ src/routes/apps/lord-ledger/privacy/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import PrivacyPage from '$lib/PrivacyPage.svelte';
+	import { lordLedgerPrivacyContent } from '$lib/lordLedgerPrivacyContent';
+</script>
+
+<PrivacyPage
+	localeKey="ko"
+	contentOverride={lordLedgerPrivacyContent.ko}
+	productSlug="lord-ledger"
+/>
---
+++ src/routes/en/apps/lord-ledger/privacy/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import PrivacyPage from '$lib/PrivacyPage.svelte';
+	import { lordLedgerPrivacyContent } from '$lib/lordLedgerPrivacyContent';
+</script>
+
+<PrivacyPage
+	localeKey="en"
+	contentOverride={lordLedgerPrivacyContent.en}
+	productSlug="lord-ledger"
+/>
